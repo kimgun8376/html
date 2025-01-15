708fec99ac5280a93194d0010d84a1c2dd6a4990
kimgun8376 <dbasdf48287@gamil.com>
2025-01-15 17:26:05 +0900

    cimmit

--- javascript/loop/whileloop3.js
+++ javascript/loop/whileloop3.js
@@ -1,34 +1,49 @@
-// whileloo2.js
-// stop을 입력하면 반복문 종료.
-// 입력한 값중에서 제일 큰 값을 출력.
+// whileloop3.js
+// Object 타입
 
+let myInfo ={
+  name:"홍길동",
+  age:20,
+  height:170,
+  weight:65
+}; // onject
+console.log(myInfo);
+console.log(`이름:${myInfo.name},나이:${myInfo['age']}`);
+console.log(`키:${myInfo.height},몸무게:${myInfo['weight']}`);
+//for..in 반복문.
+for(let prop in myInfo) {
+  console.log(`속성:${prop}, 값:${myInfo[prop]}`); //속성,값 추가
+}
 
-//let inputValue = prompt("점수를 입력하세요");
-//console.log(inputValue);
-//while(run) {
-// let temp = p
-// arseInt(Math.random());   
-//console.log(temp);
-// if (i == stop){
-// break;
-//}s
-//}
+let nameAry = ['홍길동','김민석','박석민','김민수'];
+for(let name of nameAry) {
+  console.log(`값 : ${name}`);
+}
 
-let run = true;
-let max = 0; // 최대값
-while (run) {
-  let inputValue = prompt("점수를 입력하세요:");
-  // 입력값이 숫자인지 아닌지 구분
-  // 숫자가 아니면 => stop인지 아닌지 구분
-  // 숫자이면 => 최고점수를 구하고 
-  console.log(inputValue);
-  //let temp = parseInt(inputValue); //최고점수만 출력
-  if (inputValue == 'stop') {
-    run = false;
-  } else {
-    if (max < inputValue) {
-      max = inputValue
-    }
-  }
-} //end of while.
-console.log(`최고점수: ${max}`);
+let myFriend1 = {
+  name: "홍길동",
+  phone: "010-1111-2222",
+  address :"대구 중구 100번지"
+}
+
+let myFriend2 = {
+  name: "김민수",
+  phone: "010-3333-4444",
+  address :"대구 중구 200번지"
+}
+
+let myFriend3 = {
+  name: "박석민",
+  phone: "010-5555-6666",
+  address :"대구 중구 400번지"
+}
+
+let friends = [myFriend1, myFriend2, myFriend3];
+friends[3] = {name: "박창석", phone:"010-1234-1234",address:"대구 북구 100번지"}
+console.log(friends[0].name);
+console.log(friends[1]['phone']);
+console.log(friends[2]['address']);
+for(let i=0; i < friends.length; i++){
+  //친구이름.
+  console.log(`이름: ${friends[i].name},연락처 :${friends[i]['phone']}`);
+}
---
+++ javascript/loop/whileloop5.js
@@ -0,0 +1,43 @@
+// whileloop5.js
+
+let members = [];
+members[0] = {member_id:"user01", member_name :"홍길동", point:95};
+members[1] = {member_id:"user02", member_name :"신현욱", point:90};
+members[2] = {member_id:"user03", member_name : "김민식", point:80};
+
+// 입력후 실패시 없습니다. 문구 출력
+
+//let 내친구 = '김민식' //변수등록 
+while (true) {
+  let searchName = prompt("찾을 친구 이름 입력:");
+  if (searchName == 'stop') {
+    break; //while 반복문 종료
+  }
+  let exists = false; //존재여부 담아놓기.
+  for (let i = 0; i < members.length; i++) {
+    // 배열의 이름속성중에서 searchName 과 같으면 포인트를 콘솔출력
+    if (members[i].member_name == searchName) {
+      console.log(`${searchName}의 포인트는 ${members[i].point}`);
+      exists = false;
+    }
+    if (!exists) {
+      alert("찾는 친구이름이 없습니다.");
+    }
+  } // end of while.
+  console.log('end of program.');
+}
+
+
+let temp = 0;
+while(temp < 3) {
+  let searchName = prompt("찾을 친구 이름 입력:");
+    //배열의 이름속성중에서 searchName과 같으면 포인트를 콘솔출력.
+    if (members[temp].member_name == searchName ) {
+      console.log(`${searchName}의 포인트는 ${members[temp].point}`);
+      break;
+    } else {
+      alert('없습니다.');
+      break;
+    } temp ++
+  } 
+  
