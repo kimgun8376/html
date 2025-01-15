881bb595d84af76d170a979a84e789179f282865
kimgunoh <dbasdf48287@gmail.com>
2025-01-15 22:02:52 +0900

    커밋밋

--- javascript/bascic/todol.js
+++ javascript/bascic/todol.js
@@ -1,2 +1,15 @@
 // todo1.js
 //3개숫자에서 가장 작은거 
+
+let num1 = parseInt(Math.random());
+let num2 = parseInt(Math.random());
+let num3 = parseInt(Math.random());
+let min;
+console.log(num1,num2,num3) 
+    if (num1<num2) {
+    console.log(nmix=num2);
+     } else{
+      console.log( min=num2); 
+     }if(num3<min) {
+    console.log(min=num3);
+     }
