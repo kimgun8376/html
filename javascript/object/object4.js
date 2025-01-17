18c670347a091be7bce026d5319767d4bf5c4975
kimgun8376 <dbasdf48287@gamil.com>
2025-01-17 16:50:16 +0900

    cc

--- javascript/object/object4.js
+++ javascript/object/object4.js
@@ -4,7 +4,14 @@ let month = 2;
 let date = 0;
 let year = 2024;
 
+console.clear();
+let a = 'hello';
+let b = 'hello';
+console.log(a == b)
+
 let today = new Date();
+let now = new Date();
+console.log(today.getTime() == now.getTime());
 
 today.setFullYear(year);  //년 변경 
 today.setMonth(month); //달 변경 
@@ -12,7 +19,15 @@ today.setDate(date); // 날짜 변경
 today = new Date(2024, 4, 3, 15, 30, 22);
 
 console.log(today, today.getDate());
+console.log(new Date(2000));
+console.log(today.valueOf() == now.valueOf());
+console.log(today.valueOf());
+
+
 
 today = new Date();
 // 2022년 5월 
+today.setFullYear(2025);  //년 변경 
+today.setMonth(4); //달 변경 
+today.setDate(25); // 날짜 변경
 console.log(today);
